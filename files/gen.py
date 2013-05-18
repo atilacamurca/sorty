642758b3bb9c817b92d05437ea89b9115efff343
atilacamurca <camurca.home@gmail.com>
2013-05-18 10:54:15 -0300

    gen files with max value. gen_results skip files to slow algorithms.

--- files/gen.py
+++ files/gen.py
@@ -3,16 +3,25 @@
 
 import sys
 import random
+import os
 
-def create_file(tam):
-   f = open("array-file-%d.txt" % tam, "w")
+MAX = 100
+
+def create_file(size):
+   f = open("array-file-%d.txt" % size, "w")
    # write tam in the first line, so the program can know size of array
-   f.write("%d\n" % tam)
-   for i in range(0, tam):
-      f.write("%d\n" % random.randrange(0,tam))
+   f.write("%d\n" % size)
+   for i in range(0, size):
+      f.write("%d\n" % random.randrange(0,MAX))
    
    f.close()
 
 if __name__ == '__main__':
-   tam = int(sys.argv[1])
-   create_file(tam)
+   if len(sys.argv) == 1:
+      print "usage: ./gen.py size"
+      exit(1)
+   
+   os.system("rm array-file-*")
+   size = int(sys.argv[1])
+   for i in range(0, size):
+      create_file(10 ** (i + 1))
--- gen_results.py
+++ gen_results.py
@@ -3,6 +3,8 @@
 
 import os
 
+ARRAY_SIZE = 6
+
 if __name__ == '__main__':
    data = {
       0: 'bubble',
@@ -12,10 +14,15 @@ if __name__ == '__main__':
       4: 'counting',
       5: 'bigger smaller'
    }
+   # clear results/ directory
    os.system("rm results/*")
    for i in range(0, len(data)):
-      for j in range(0, 3):
+      for j in range(0, ARRAY_SIZE):
+         if (i == 0 or i == 1 or i == 2 or i == 5) and j == ARRAY_SIZE - 1:
+            continue
+			
          count = 10 ** (j + 1)
          for k in range(0, 5):
             print "alg:%s size:%d iter:%d file:%d-%d" % (data[i], count, k, i, count)
-            os.system("time -a -o results/%d-%d.txt -f \"%%E\" ./sorty sort %d %d" % (i, count, i, count))
+            os.system("time -a -o results/%d-%d.txt -f \"%%e\" ./sorty sort %d %d" % (i, count, i, count))
+
